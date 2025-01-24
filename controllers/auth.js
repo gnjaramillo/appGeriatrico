95cf4eed84995ab36fa22e074c75e4eb5522fe21
gnjaramillo <gnjaramillo1986@gmail.com>
2025-01-24 14:33:33 -0500

    cambios de forma

--- controllers/auth.js
+++ controllers/auth.js
@@ -7,20 +7,18 @@ const { subirImagenACloudinary } = require('../utils/handleCloudinary');
 const cloudinary = require('../config/cloudinary'); 
 
 
-// controllers/personasController.js
 const registrarPersona = async (req, res) => {
     try {
-        // Obtener los datos validados del formulario
         const data = matchedData(req);
         const { per_correo, per_documento, per_nombre_completo, per_telefono, per_genero, per_usuario, per_password } = data;
 
-        // Validar si el correo ya está registrado
+        // Validar correo 
         const correoExiste = await personaModel.findOne({ where: { per_correo } });
         if (correoExiste) {
             return res.status(400).json({ message: 'El correo ya está registrado' });
         }
 
-        // Validar si el documento ya está registrado
+        // Validar documento
         const documentoExiste = await personaModel.findOne({ where: { per_documento } });
         if (documentoExiste) {
             return res.status(400).json({ message: 'El documento ya está registrado' });
@@ -29,7 +27,7 @@ const registrarPersona = async (req, res) => {
         // Encriptar la contraseña del usuario
         const per_password_encriptada = await encrypt(per_password);
 
-        console.log("Contraseña recibida:", per_password_encriptada);
+        // console.log("Contraseña recibida:", per_password_encriptada);
 
 
         // Crear la persona en la base de datos
@@ -43,12 +41,12 @@ const registrarPersona = async (req, res) => {
             per_password: per_password_encriptada, 
         });
 
-        // Manejar la subida de la foto (opcional)
+        // subir foto (foto es opcional en registro)
         let per_foto = null;
         if (req.file) {
-            // Subir la imagen a Cloudinary (función que ya tienes configurada)
+            // Subir la imagen a Cloudinary 
             const resultado = await subirImagenACloudinary(req.file, "personas");
-            per_foto = resultado.secure_url; // URL de la imagen subida
+            per_foto = resultado.secure_url; // URL imagen 
 
             nuevaPersona.per_foto = per_foto;
             await nuevaPersona.save();
@@ -57,6 +55,7 @@ const registrarPersona = async (req, res) => {
 
         // Ocultar la contraseña en la respuesta para no exponerla
         nuevaPersona.set('per_contraseña', undefined, { strict: false });
+
 
         // Responder con éxito y devolver los datos del usuario y el token
         return res.status(201).json({
@@ -112,9 +111,10 @@ const loginPersona = async (req, res) => {
       },
     });
 
-    // Guardar en la sesión el rol o si es super admin
-    req.session.rol_id = esSuperAdmin ? 1 : null; // Guardar el rol si no es Super Admin
-    req.session.esSuperAdmin = !!esSuperAdmin; // Booleano indicando si es Super Admin
+    // Guardar en la sesión el rol si es super admin
+    req.session.rol_id = esSuperAdmin ? 1 : null; 
+    req.session.esSuperAdmin = !!esSuperAdmin; 
+
 
     console.log("rol_id en la sesión:", req.session.rol_id);
 
@@ -122,7 +122,7 @@ const loginPersona = async (req, res) => {
     const token = await tokenSign({
       per_id: persona.per_id,
       per_nombre_completo: persona.per_nombre_completo,
-      esSuperAdmin: !!esSuperAdmin, // true si es Super Admin, false si no
+      esSuperAdmin: !!esSuperAdmin, // Booleano indicando si es Super Admin
     });
 
     // **Guardar el token en las cookies**
@@ -158,58 +158,7 @@ const loginPersona = async (req, res) => {
 
 
 
-
-
-/*   const loginPersona = async (req, res) => {
-    try {
-        const data = matchedData(req);
-        const { per_usuario, per_password } = data;
-
-        // Buscar al usuario
-        const persona = await personaModel.findOne({ where: { per_usuario } });
-        if (!persona) {
-            return res.status(404).json({ message: 'Usuario no encontrado' });
-        }
-
-        // Verificar contraseña
-        const hashPassword = persona.per_password;
-        const check = await compare(per_password, hashPassword);
-        if (!check) {
-            return res.status(401).json({ message: 'Contraseña inválida' });
-        }
-
-        persona.set("password", undefined, { estrict: false });
-
-
-        // Generar el token básico con los datos del usuario
-        const token = await tokenSign({
-            per_id: persona.per_id,
-            per_nombre_completo: persona.per_nombre_completo,
-        });
-
-        // Responder con el mensaje y las opciones
-        return res.status(200).json({
-            message: 'Inicio de sesión exitoso',
-            token,
-            persona: {
-                id: persona.per_id,
-                nombre: persona.per_nombre_completo,
-                usuario: persona.per_usuario,
-                correo: persona.per_correo,
-                foto: persona.per_foto,
-                genero: persona.per_genero,
-                telefono: persona.per_telefono
-            }
-        });
-
-    } catch (error) {
-        console.error('Error en el login:', error);
-        return res.status(500).json({ message: 'Error en el login' });
-    }
-};
-*/
-
-  module.exports = { registrarPersona, loginPersona };
+module.exports = { registrarPersona, loginPersona };
 
 
 
