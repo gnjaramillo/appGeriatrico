2c9015ea09d106fd0aad5a3f0a0cf96c31e926ef
Guisella N. Jaramillo Portillo <gnjaramillo1986@gmail.com>
2025-04-08 10:11:38 -0500

    controladores formulacion medica

--- validators/formulacionmedicamentos.js
+++ validators/formulacionmedicamentos.js
@@ -12,6 +12,8 @@ const validatorCrearFormulacion = [
       .isInt().withMessage('El ID del paciente debe ser un número entero')
       .exists().notEmpty(),
   
+
+
     // Validar que existan los campos requeridos
     check([
       "med_id",
@@ -25,6 +27,9 @@ const validatorCrearFormulacion = [
       .exists().withMessage("Todos los campos son obligatorios.")
       .notEmpty().withMessage("Los campos no pueden estar vacíos."),
   
+
+
+
     // Validar fechas
     check("admin_fecha_inicio")
       .custom((value) => {
@@ -41,6 +46,9 @@ const validatorCrearFormulacion = [
   
         return true;
       }),
+
+
+
   
     check("admin_fecha_fin")
       .custom((value, { req }) => {
@@ -58,24 +66,35 @@ const validatorCrearFormulacion = [
         return true;
       }),
   
+
+
+
     // Validar dosis por toma
     check("admin_dosis_por_toma")
       .isFloat({ min: 0.01 }).withMessage("La dosis por toma debe ser un número positivo."),
+
+
   
     // Validar tipo de cantidad (unidades, mililitros, gramos, gotas)
     check("admin_tipo_cantidad")
-      .isIn(["unidades", "mililitros", "gramos", "gotas"])
+      .isIn(["unidades", "mililitros", "gramos", "gotas", "otro"])
       .withMessage("El tipo de cantidad no es válido."),
+
+
   
     // Validar formato de hora 24h
     check("admin_hora")
       .matches(/^([01]\d|2[0-3]):([0-5]\d)$/)
       .withMessage("La hora debe estar en formato HH:MM (24 horas)."),
+
+
   
     // Validar método de administración
     check("admin_metodo")
-      .isIn(["Oral", "Intravenosa", "Subcutánea", "Tópica", "Inhalación"])
+      .isIn(["Oral", "Intravenosa", "Subcutánea", "Tópica", "Inhalación", "Rectal", "Ótica", "Oftálmica", "Nasal", "Otro" ])
       .withMessage("Método de administración no válido."),
+
+
   
     // Validar estado solo si viene (debe ser 'Pendiente' o 'Suspendido')
 /*     check("admin_estado")
@@ -83,7 +102,7 @@ const validatorCrearFormulacion = [
       .isIn(["Pendiente", "Suspendido"])
       .withMessage("El estado solo puede ser 'Pendiente' o 'Suspendido'."), */
 
-      
+
   
     (req, res, next) => validateResult(req, res, next),
   ];
